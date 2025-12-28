ae18a0b968501a9db2ec65e201049f8b51af99e6
OviniP <ovinipoornima@gmail.com>
2025-12-28 06:41:02 +0200

    Done categories routes

--- routes/web.php
+++ routes/web.php
@@ -5,4 +5,13 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
-Route::get('/categories', [CategoryController::class, 'index'])->name('categories.index');
+
+Route::prefix('categories')->name('categories.')->group(function () {
+    Route::get('/', [CategoryController::class, 'index'])->name('index');
+    Route::get('/create', [CategoryController::class, 'create'])->name('create');
+    Route::post('/', [CategoryController::class, 'store'])->name('store');
+    Route::get('/{category}/edit', [CategoryController::class, 'edit'])->name('edit');
+    Route::put('/{category}', [CategoryController::class, 'update'])->name('update');
+    Route::delete('/{category}', [CategoryController::class, 'delete'])->name('delete');
+    Route::get('/{category}/status', [CategoryController::class, 'status'])->name('status');
+});
